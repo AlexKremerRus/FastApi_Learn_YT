1f8c21877bfb25663dbddbb56651ae5b6f6bdacf
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2024-08-10 22:23:26 +0300

    add new method

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello World"}
+
+@app.get("/{name}")
+def get_name(name: int, state: str = None):
+    if state is None:
+        return {"message": f"Hello {name}"}
+    else:
+        return {"message": f"Hello {name} - {state}"}
