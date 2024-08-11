851ac6634d68e81676c0c5f6121e024ad70befa1
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2024-08-11 14:13:57 +0300

    add method with query and path

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from schemas import Book
 from typing import List
 
@@ -17,16 +17,23 @@ def get_name(name: int, state: str = None):
     else:
         return {"message": f"Hello {name} - {state}"}
 
-
+# работа с path параметрами
 @app.get("/user/{name}/item/{item}")
 def get_user_item(name: str, item: int):
     return {"message": f"Hello {name} - {item}"}
 
+# работа с body и post параметрами
 @app.post("/book")
 def create_book(item: Book):
     return item
 
+# работа с query параметрами
 @app.get("/books")
 def get_books_test(e: List[str] = Query( description='search book')):  #, min_length=2, max_length=5
     print(e)
     return {"e": e}
+
+# работа с query параметрами и Path параметрами
+@app.get("/book/{book_id}")
+def get_book(book_id: int = Path(..., gt=1, le=5), pages: int = Query(None, gt=10, lt=100)):
+    return {"book_id": book_id, "pages": pages}
